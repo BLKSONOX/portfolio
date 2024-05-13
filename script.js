15dab4ab952afd0320b7312863453cddf1a31926
BLKSONOX <166595586+BLKSONOX@users.noreply.github.com>
2024-05-13 16:31:31 +0200

    notes changes

--- script.js
+++ script.js
@@ -112,7 +112,7 @@ ____ = {
             
             */
 
-            /* How am operating system kernel is made, and how it's routed */
+            /* How an operating system kernel is made, and how it's routed */
             /* Event rotational objects and functions */
 
 

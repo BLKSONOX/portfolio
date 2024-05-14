5506735b76189d1581677115c3a60ae09d173813
BLKSONOX <166595586+BLKSONOX@users.noreply.github.com>
2024-05-14 18:15:22 +0200

    changes notes

--- script.js
+++ script.js
@@ -126,7 +126,7 @@ ____ = {
                 usually through special characters expression |*@£$$€{}
                 tied to keyboard mappings
 
-                a kernels test drive on a hardware is;z
+                a kernels test drive on a hardware is
                 run as many mapped special character channels multiplied also known as threads
                 why special characters are reserved in programming languages CPU profiling
                 this is what became the ISO standards, later unicode

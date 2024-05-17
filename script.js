f3ff7fcd6e275904a0b45f6458bdf41e7e78a938
BLKSONOX <166595586+BLKSONOX@users.noreply.github.com>
2024-05-17 11:33:00 +0200

    changes notes

--- script.js
+++ script.js
@@ -339,6 +339,11 @@ ____ = {
 
                 Elementary and advanced Algebra
 
+                in the first is moves to in the 1.5 according to numberpositions and composition of 
+                the whole mathemathical equation
+
+                exponentes are still number tables and positional points
+
                 1x8(2) + 2x8(1) + 1x8(0) ------ number rows in reverse, in the upper (0), (1.5), (2) ~ 8(0), 16(1.5), 8(2) ~~ 8, 24, 16 ~~ 24 hyperbola /\ (row repeats four times) 0240
 
                 24+24+24+24

fb3740c214bd772af2a18972ad75c224bc2ffca1
BLKSONOX <166595586+BLKSONOX@users.noreply.github.com>
2024-05-18 11:09:04 +0200

    Changes notes

--- script.js
+++ script.js
@@ -347,7 +347,12 @@ ____ = {
 
                 flipping is also allowed within the expressional equation set
 
-                1x8(2) + 2x8(1) + 1x8(0) ------ number rows in reverse, in the upper (0), (1.5), (2) ~ 8(0), 16(1.5), 8(2) ~~ 8, 24, 16 ~~ 24 hyperbola /\ (row repeats four times) 0240
+                1x8(2) + 2x8(1) + 1x8(0) ------ number rows in reverse, in the upper 
+                (0), (1.5), (2) ~ 8(0), 16(1.5), 8(2) ~~ 8, 24, 16 ~~ 24 hyperbola /\ (row repeats four times) 0240
+
+                https://en.wikipedia.org/wiki/Compact-open_topology C(X,Y)
+
+                Directly natural transformation towards hyperbola 
 
                 https://en.wikipedia.org/wiki/Hyperbola
 

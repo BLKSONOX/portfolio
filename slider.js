e0e627677612d0ac41cfaadc9fef2c2756ddf375
BLKSONOX <166595586+BLKSONOX@users.noreply.github.com>
2024-05-29 05:38:56 +0200

    changes

--- slider.js
+++ slider.js
@@ -7,42 +7,50 @@ slider = {
     init:{
         _(){
 
-            setTimeout(function(){
+            setInterval(function(){
+            
+                const date = new Date();
+                console.log(date.toLocaleTimeString());
+
+                // Do a timed repeat for loop or event action call
+                // not quite done with this
+    
                 const slide1 = document.getElementById("slide1");
                 const slide2 = document.getElementById("slide2");
-
+    
                 slide2.classList.remove("hidden");
                 slide2.classList.add("show");
-
+    
                 slide1.classList.remove("show");
                 slide1.classList.add("hidden");
-
+    
                 setTimeout(function(){
                     const slide3 = document.getElementById("slide3");
                     const slide2 = document.getElementById("slide2");
-
+    
                     slide3.classList.remove("hidden");
                     slide3.classList.add("show");
-
+    
                     slide2.classList.remove("show");
                     slide2.classList.add("hidden");
-
+    
                     setTimeout(function(){
                         // The end is the begining
                         const slide3 = document.getElementById("slide3");
                         const slide1 = document.getElementById("slide1");
-
+    
                         slide1.classList.remove("hidden");
                         slide1.classList.add("show");
-
+    
                         slide3.classList.remove("show");
                         slide3.classList.add("hidden");
                         
                     }, self.slider.slideTime);
-
+    
                 },  self.slider.slideTime);
                 
             }, self.slider.slideTime);
+
 
         }
     }
@@ -50,5 +58,4 @@ slider = {
 
 window.onload = function(){
     slider.init._();
-    // Do a timed repeat for loop or event action call
 }

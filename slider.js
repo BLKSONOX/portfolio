7773344fe3e2994a7bf609ddb2de067bcc23de35
BLKSONOX <166595586+BLKSONOX@users.noreply.github.com>
2024-05-29 04:42:16 +0200

    Added slider, needs more testing

---
+++ slider.js
@@ -0,0 +1,53 @@
+
+
+slider = {
+
+    slideTime: 5000,
+
+    init:{
+        _(){
+
+            setTimeout(function(){
+                const slide1 = document.getElementById("slide1");
+                const slide2 = document.getElementById("slide2");
+
+                slide2.classList.remove("hidden");
+                slide2.classList.add("show");
+
+                slide1.classList.remove("show");
+                slide1.classList.add("hidden");
+
+                setTimeout(function(){
+                    const slide3 = document.getElementById("slide3");
+                    const slide2 = document.getElementById("slide2");
+
+                    slide3.classList.remove("hidden");
+                    slide3.classList.add("show");
+
+                    slide2.classList.remove("show");
+                    slide2.classList.add("hidden");
+
+                    setTimeout(function(){
+                        // The end is the begining
+                        const slide3 = document.getElementById("slide3");
+                        const slide1 = document.getElementById("slide1");
+
+                        slide1.classList.remove("hidden");
+                        slide1.classList.add("show");
+
+                        slide3.classList.remove("show");
+                        slide3.classList.add("hidden");
+                        
+                    }, self.slider.slideTime);
+
+                },  self.slider.slideTime);
+                
+            }, self.slider.slideTime);
+
+        }
+    }
+}
+
+window.onload = function(){
+    slider.init._();
+}
